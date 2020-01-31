875b21344f65cd610b74034145ff2d7afa016dbe
Daniel Refson <daniel.refson@fanatics.co.uk>
2020-01-31 12:50:08 +0000

    tweaked database -migrations

---
+++ HolidaySlug.Data/Migrations/20200131124416_ResolvedUserTeamRelationship.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolidaySlug.Data.Migrations
+{
+    public partial class ResolvedUserTeamRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserTeams_UserTeamID",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "UserTeams",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "UserTeamID",
+                table: "Users",
+                newName: "UserTeamId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_UserTeamID",
+                table: "Users",
+                newName: "IX_Users_UserTeamId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserID",
+                table: "UserTeams",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTeams_UserID",
+                table: "UserTeams",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserTeams_UserTeamId",
+                table: "Users",
+                column: "UserTeamId",
+                principalTable: "UserTeams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTeams_Users_UserID",
+                table: "UserTeams",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserTeams_UserTeamId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTeams_Users_UserID",
+                table: "UserTeams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserTeams_UserID",
+                table: "UserTeams");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "UserTeams");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserTeams",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "UserTeamId",
+                table: "Users",
+                newName: "UserTeamID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_UserTeamId",
+                table: "Users",
+                newName: "IX_Users_UserTeamID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserTeams_UserTeamID",
+                table: "Users",
+                column: "UserTeamID",
+                principalTable: "UserTeams",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ HolidaySlug.Data/Migrations/20200131124824_addedRequiredUser_Holiday_addedIsManager_User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolidaySlug.Data.Migrations
+{
+    public partial class addedRequiredUser_Holiday_addedIsManager_User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Holidays_Users_UserID",
+                table: "Holidays");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsManager",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "Holidays",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Holidays_Users_UserID",
+                table: "Holidays",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Holidays_Users_UserID",
+                table: "Holidays");
+
+            migrationBuilder.DropColumn(
+                name: "IsManager",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "Holidays",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Holidays_Users_UserID",
+                table: "Holidays",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
